08b026efc16d57bfa4cf246e9f74150038f6e3a6
Steven J. Selcuk <stevenjselcuk@gmail.com>
2020-05-08 13:08:38 +0300

    Initial Commit

---
+++ AnlatBakalim/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnlatBakalim
+//
+//  Created by Steven J. Selcuk on 8.05.2020.
+//  Copyright © 2020 Steven J. Selcuk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
